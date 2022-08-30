4f9f214dce7eab19be61cf3c3f1e4315f3a36d2b
Chinchilla7 <regina.chen@stonybrook.edu>
2022-08-30 16:17:05 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,1 +1,18 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return 'Hello world!'
+
+@app.route('/contactus')
+def contact_page():
+    return '911'
+
+@app.route('/careers')
+def careers_page():
+    return 'seeking employees'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8000)
