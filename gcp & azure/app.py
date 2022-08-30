a601cd8b7fdc7a76abd0644a2c62ca67c81a77f7
Chinchilla7 <regina.chen@stonybrook.edu>
2022-08-30 17:02:05 -0400

    Update app.py

--- gcp & azure/app.py
+++ gcp & azure/app.py
@@ -15,4 +15,4 @@ def careers_page():
     return 'seeking employees'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=80)
